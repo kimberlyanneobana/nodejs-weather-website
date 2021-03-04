62fa5dbb1c9379a0308e2b31f71cc868a7fd9292
kimberlyanneobana <kimberly.anne.obana@accenture.com>
2021-03-04 19:40:27 +0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -102,6 +103,12 @@ app.get('*', (req, res) => {
     errorMessage: 'Page not found.',
   });
 });
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+
+// for local
+// app.listen(3000, () => {
+//   console.log('Server is up on port 3000.');
+// });
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
